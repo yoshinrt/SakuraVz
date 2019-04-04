2af6695d646c5f166f74a09c948897ceb64fdd9c
Yoshi NRT <32790691+yoshinrt@users.noreply.github.com>
2019-04-04 20:25:02 +0900

    Revert "メールアドレスにはスペースを含まないことを前提に修正"
    This reverts commit 6e7a4df5de2889026e9e8cd4c47ba80974408008.

--- sakura_core/util/string_ex.cpp
+++ sakura_core/util/string_ex.cpp
@@ -1115,9 +1115,6 @@ inline static bool IsMailAddressLocalPart(
 				return true; // ここは準正常終了。正常終了とはあえて区別しない。
 			}
 			return false; // 末尾以外に現れるエスケープされてない二重引用符は不正
-		case L' ': // スペース
-		case L'\t':
-			return false;
 		}
 		pszScan++;
 	}
