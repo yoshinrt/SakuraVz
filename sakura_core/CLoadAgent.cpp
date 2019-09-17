31ddb3db53ab171009be0923e583a59a6be2d510
Yoshi NRT <32790691+yoshinrt@users.noreply.github.com>
2019-09-17 21:24:03 +0900

    画面右で折返し設定している場合，DoLayout が 2回走るのをやめる #98

--- sakura_core/CLoadAgent.cpp
+++ sakura_core/CLoadAgent.cpp
@@ -264,7 +264,10 @@ ELoadResult CLoadAgent::OnLoad(const SLoadInfo& sLoadInfo)
 		nMaxLineKetas = CKetaXInt(MAXLINEKETAS);
 
 	CProgressSubject* pOld = CEditApp::getInstance()->m_pcVisualProgress->CProgressListener::Listen(&pcDoc->m_cLayoutMgr);
-	pcDoc->m_cLayoutMgr.SetLayoutInfo( true, true, ref, ref.m_nTabSpace, ref.m_nTsvMode, nMaxLineKetas, CLayoutXInt(-1), &pcDoc->m_pcEditWnd->GetLogfont() );
+	pcDoc->m_cLayoutMgr.SetLayoutInfo(
+		ref.m_nTextWrapMethod != WRAP_WINDOW_WIDTH || pcDoc->m_cDocFile.m_sFileInfo.IsLargeFile(),
+		true, ref, ref.m_nTabSpace, ref.m_nTsvMode, nMaxLineKetas, CLayoutXInt(-1), &pcDoc->m_pcEditWnd->GetLogfont()
+	);
 	pcDoc->m_pcEditWnd->ClearViewCaretPosInfo();
 	if (pcDoc->m_cLayoutMgr.m_tsvInfo.m_nTsvMode != TSV_MODE_NONE) {
 		pcDoc->m_cLayoutMgr.m_tsvInfo.CalcTabLength(pcDoc->m_cLayoutMgr.m_pcDocLineMgr);
