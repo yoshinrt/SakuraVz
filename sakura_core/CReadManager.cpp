cbaf48dc39f1456bef6f7e29fcfca1f016ec3280
Yoshi NRT <32790691+yoshinrt@users.noreply.github.com>
2020-02-09 18:03:45 +0900

    UTF-16 で GetNextLineTop が不正な結果を返すのを修正 #109

--- sakura_core/CReadManager.cpp
+++ sakura_core/CReadManager.cpp
@@ -115,7 +115,7 @@ EConvertResult CReadManager::ReadFile_To_CDocLineMgr(
 				uBeginPos = 0;
 			}else{
 				uBeginPos = cfl[ iThreadID ].GetNextLineTop(
-					cfl[ iThreadID ].GetFileSize() * iThreadID / uMaxThreadNum
+					( cfl[ iThreadID ].GetFileSize() * iThreadID / uMaxThreadNum ) & ~1
 				);
 			}
 			
@@ -124,7 +124,7 @@ EConvertResult CReadManager::ReadFile_To_CDocLineMgr(
 				uEndPos = cfl[ iThreadID ].GetFileSize();
 			}else{
 				uEndPos = cfl[ iThreadID ].GetNextLineTop(
-					cfl[ iThreadID ].GetFileSize() * ( iThreadID + 1 ) / uMaxThreadNum
+					( cfl[ iThreadID ].GetFileSize() * ( iThreadID + 1 ) / uMaxThreadNum ) & ~1
 				);
 			}
 			
