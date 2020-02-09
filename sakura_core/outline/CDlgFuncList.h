4032e4194f5fcb82af2891c9ed802a4f2a7b541f
Yoshi NRT <32790691+yoshinrt@users.noreply.github.com>
2020-02-09 17:25:38 +0900

    Revert "C++ でアウトラインがトグルしないのを修正 #90"
    This reverts commit 120b8f2300371fe993dc0f7580ce7d328865b1fe.

--- sakura_core/outline/CDlgFuncList.h
+++ sakura_core/outline/CDlgFuncList.h
@@ -90,13 +90,7 @@ protected:
 public:
 	/*! 現在の種別と同じなら
 	*/
-	bool CheckListType( int nOutLineType ) const {
-		return nOutLineType == m_nOutlineType ||
-			nOutLineType == OUTLINE_C_CPP && (
-				m_nOutlineType == OUTLINE_C ||
-				m_nOutlineType == OUTLINE_CPP
-			);
-	}
+	bool CheckListType( int nOutLineType ) const { return nOutLineType == m_nOutlineType; }
 	void Redraw( int nOutLineType, int nListType, CFuncInfoArr*, CLayoutInt nCurLine, CLayoutInt nCurCol );
 	void Refresh( void );
 	bool ChangeLayout( int nId );
