710905f2c0da57b86cc1b29f004b5ff55f70659b
Yoshi NRT <32790691+yoshinrt@users.noreply.github.com>
2025-06-06 00:04:14 +0900

    CMemPool 使用を再適用
    This reverts commit e8fda0268386dfafa72c634ded137cc68a7654d9.

--- sakura_core/doc/layout/CLayoutMgr.cpp
+++ sakura_core/doc/layout/CLayoutMgr.cpp
@@ -26,6 +26,7 @@
 #include "charset/charcode.h"
 #include "mem/CMemory.h"/// 2002/2/10 aroka
 #include "mem/CMemoryIterator.h" // 2006.07.29 genta
+#include "mem/CPoolResource.h"
 #include "view/CViewFont.h"
 #include "view/CTextMetrics.h"
 #include "basis/SakuraBasis.h"
@@ -41,6 +42,8 @@
 
 CLayoutMgr::CLayoutMgr()
 : m_getIndentOffset( &CLayoutMgr::getIndentOffset_Normal )	//	Oct. 1, 2002 genta	//	Nov. 16, 2002 メンバー関数ポインタにはクラス名が必要
+  , m_layoutMemRes(new CPoolResource<CLayout>())
+  //, m_layoutMemRes(new std::pmr::unsynchronized_pool_resource()) // メモリ使用量が大きい為に使用しない
 {
 	m_pcDocLineMgr = NULL;
 	m_pTypeConfig = NULL;
@@ -99,7 +102,8 @@ void CLayoutMgr::_Empty()
 	CLayout* pLayout = m_pLayoutTop;
 	while( pLayout ){
 		CLayout* pLayoutNext = pLayout->GetNextLayout();
-		delete pLayout;
+		pLayout->~CLayout();
+		m_layoutMemRes->deallocate(pLayout, sizeof(CLayout), alignof(CLayout));
 		pLayout = pLayoutNext;
 	}
 }
@@ -386,7 +390,7 @@ CLayout* CLayoutMgr::CreateLayout(
 	CLayoutColorInfo*	colorInfo
 )
 {
-	CLayout* pLayout = new CLayout(
+	CLayout* pLayout = new (m_layoutMemRes->allocate(sizeof(CLayout))) CLayout(
 		pCDocLine,
 		ptLogicPos,
 		nLength,
@@ -594,7 +598,8 @@ CLayout* CLayoutMgr::DeleteLayoutAsLogical(
 			DEBUG_TRACE( L"バグバグ\n" );
 		}
 
-		delete pLayout;
+		pLayout->~CLayout();
+		m_layoutMemRes->deallocate(pLayout, sizeof(CLayout), alignof(CLayout));
 
 		m_nLines--;	/* 全物理行数 */
 		if( NULL == pLayoutNext ){
--- sakura_core/doc/logic/CDocLineMgr.cpp
+++ sakura_core/doc/logic/CDocLineMgr.cpp
@@ -39,6 +39,7 @@
 //	May 15, 2000 genta
 #include "CEol.h"
 #include "mem/CMemory.h"// 2002/2/10 aroka
+#include "mem/CPoolResource.h"
 
 #include "io/CFileLoad.h" // 2002/08/30 Moca
 #include "io/CIoBridge.h"
@@ -53,6 +54,8 @@
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
 
 CDocLineMgr::CDocLineMgr()
+	: m_docLineMemRes(new CPoolResource<CDocLine>())
+	//: m_docLineMemRes(new std::pmr::unsynchronized_pool_resource()) // メモリ使用量が大きい為に使用しない
 {
 	_Init();
 }
@@ -69,7 +72,7 @@ CDocLineMgr::~CDocLineMgr()
 //! pPosの直前に新しい行を挿入
 CDocLine* CDocLineMgr::InsertNewLine(CDocLine* pPos)
 {
-	CDocLine* pcDocLineNew = new CDocLine;
+	CDocLine* pcDocLineNew = new (m_docLineMemRes->allocate(sizeof(CDocLine))) CDocLine();
 	_InsertBeforePos(pcDocLineNew,pPos);
 	return pcDocLineNew;
 }
@@ -77,7 +80,7 @@ CDocLine* CDocLineMgr::InsertNewLine(CDocLine* pPos)
 //! 最下部に新しい行を挿入
 CDocLine* CDocLineMgr::AddNewLine()
 {
-	CDocLine* pcDocLineNew = new CDocLine;
+	CDocLine* pcDocLineNew = new (m_docLineMemRes->allocate(sizeof(CDocLine))) CDocLine();
 	_PushBottom(pcDocLineNew);
 	return pcDocLineNew;
 }
@@ -88,7 +91,8 @@ void CDocLineMgr::DeleteAllLine()
 	CDocLine* pDocLine = m_pDocLineTop;
 	while( pDocLine ){
 		CDocLine* pDocLineNext = pDocLine->GetNextLine();
-		delete pDocLine;
+		pDocLine->~CDocLine();
+		m_docLineMemRes->deallocate(pDocLine, sizeof(CDocLine));
 		pDocLine = pDocLineNext;
 	}
 	_Init();
@@ -119,7 +123,8 @@ void CDocLineMgr::DeleteLine( CDocLine* pcDocLineDel )
 	}
 
 	//データ削除
-	delete pcDocLineDel;
+	pcDocLineDel->~CDocLine();
+	m_docLineMemRes->deallocate(pcDocLineDel, sizeof(CDocLine));
 
 	//行数減算
 	m_nLines--;
